5ddc462d6bfe50a8ef02f728749210430a7a514f
amirhosein vali <amirhosein.vali@yahoo.com>
2025-04-23 21:24:17 +0330

    add blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.ForeignKey('accounts.User', on_delete=models.PROTECT)
+    published_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blog/')
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
