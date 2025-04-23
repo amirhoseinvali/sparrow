fce69cb260f6815b72e72348cb6f2164a368b8af
amirhosein vali <amirhosein.vali@yahoo.com>
2025-04-23 21:22:43 +0330

    add payment models

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+    order = models.OneToOneField('orders.Order', on_delete=models.PROTECT)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    gateway = models.CharField(max_length=50)  # زرین‌پال، IDPay و ...
+    transaction_id = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)  # موفق، ناموفق، در انتظار
+    created_at = models.DateTimeField(auto_now_add=True)
