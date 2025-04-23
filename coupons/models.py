5dab7598501b1c75378d8106bdad91af8b6f2983
amirhosein vali <amirhosein.vali@yahoo.com>
2025-04-23 21:23:52 +0330

    add coupons models

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount_type = models.CharField(max_length=10, choices=[('percent', 'Percentage'), ('fixed', 'Fixed Amount')])
+    discount_value = models.DecimalField(max_digits=5, decimal_places=2)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    max_usage = models.PositiveIntegerField(default=1)
+    used_by = models.ManyToManyField('accounts.User', blank=True)
