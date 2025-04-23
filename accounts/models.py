6865d8f15564fac9eee3b1b17a6047b48a5b3da5
amirhosein vali <amirhosein.vali@yahoo.com>
2025-04-23 21:08:26 +0330

    add accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True)
+    email_verified = models.BooleanField(default=False)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profiles/', blank=True)
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    address = models.TextField()
+    postal_code = models.CharField(max_length=10)
+    is_default = models.BooleanField(default=False)
