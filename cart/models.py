168b12a476607fddb2705fd17491abcc872f0559
amirhosein vali <amirhosein.vali@yahoo.com>
2025-04-23 21:21:20 +0330

    add cart models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # قیمت در زمان افزودن به سبد
+
+    def total_price(self):
+        return self.quantity * self.price
